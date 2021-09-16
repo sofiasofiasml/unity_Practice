b5b3cfe737e8ec4ebcf3c37f1f8c86a0677ef50e
Sofia Marlop <romata1966@gmail.com>
2021-09-16 21:48:36 +0200

    Hexagonal map

--- Assets/Scripts/InitPlace.cs
+++ Assets/Scripts/InitPlace.cs
@@ -1,43 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Collections; 
 
 public class InitPlace : MonoBehaviour
 {
     public GameObject[] casillas;
     public GameObject gameObject; 
     //public int Count;
-    [SerializeField] int maphight = 4;
-    [SerializeField] int mapWidth = 4;
+    [SerializeField] int maphight = 25;
+    [SerializeField] int mapWidth = 12;
     private float tileXfloat = 20.0f; 
     private float tileZfloat = 17.0f;
+    private int rand;
+    SphereCollider myCollier; 
     // Start is called before the first frame update
     void Start()
     {
-		CreateHexTileMap();
-	}
+        //myCollier = GetComponent<SphereCollider>();
+        //myCollier.radius = 200;
+        CreateHexTileMap();
+
+    }
 
     void CreateHexTileMap() 
     {
-        for (int x = 1; x<= maphight; x++)
+        float mapXmin = -mapWidth / 2;
+        float mapXmax = mapWidth / 2;
+
+        float mapZmin = -maphight / 2;
+        float mapZmax = maphight / 2; 
+
+
+        for (float x = mapXmin; x<= mapXmax; x++)
         {
-            for (int z = 1; z<= mapWidth; z++) 
+            for (float z = mapZmin; z<= mapZmax; z++) 
             {
-				int rand = Random.Range(0, casillas.Length);
-				//Vector3 position = new Vector3(20 * k, 0, 20 * j);
-				GameObject randObject = Instantiate(casillas[0]);
-				randObject.transform.parent = gameObject.transform;
-				randObject.name = casillas[0].name;
+                int prob = Random.Range(0, 101);
+                Vector3 pos; 
+
+                if (prob < 70)
+                    rand = 0; 
+                else
+                    rand = Random.Range(1, casillas.Length);
+
+                //Vector3 position = new Vector3(20 * k, 0, 20 * j);
+                GameObject randObject = Instantiate(casillas[rand]);
+				//randObject.transform.parent = gameObject.transform;
+				randObject.name = casillas[rand].name;
                 if (z % 2 == 0)
                 {
-                    randObject.transform.position = new Vector3(x * tileXfloat, 0, z * tileZfloat);
+                   pos = new Vector3(x * tileXfloat, 0, z * tileZfloat);
                 }
                 else 
                 {
-                    randObject.transform.position = new Vector3(x * tileXfloat+ tileXfloat/2, 0, z * tileZfloat);
+                    pos = new Vector3(x * tileXfloat+ tileXfloat/2, 0, z * tileZfloat);
                 }
+                StartCoroutine(setTitleInfo(randObject, pos)); 
             }
         }
     }
-    
+    IEnumerator setTitleInfo(GameObject GO, Vector3 pos) 
+    {
+        yield return new WaitForSeconds(0.00001f); 
+        GO.transform.position = pos; 
+    }
+	void OnTriggerExit(Collider other)
+	{
+        Destroy(other.gameObject); 
+	}
+
+	
 }
